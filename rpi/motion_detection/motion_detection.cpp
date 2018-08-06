70d0d69de16ac3ce9829fb6869c2d1c3b879a1c0
Priyanka Patil <pripatil@uw.edu>
2018-08-05 21:05:55 -0700

    webapp added

---
+++ rpi/motion_detection/motion_detection.cpp
@@ -0,0 +1,194 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <time.h>
+#include <wiringPiSPI.h>
+#include <unistd.h>
+
+#include <ctype.h>
+#include <stdio.h>
+#include <iostream>
+#include <chrono>
+
+#include <opencv2/opencv.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/videoio.hpp>
+
+#include "arducam.h"
+
+#define OV2640_CHIPID_HIGH 0x0A
+#define OV2640_CHIPID_LOW 0x0B
+
+#define BUF_SIZE (384*1024)
+
+uint8_t buffer[BUF_SIZE] = {0xFF};
+
+void setup()
+{
+    uint8_t vid,pid;
+    uint8_t temp;
+    wiring_init();
+    arducam(smOV2640,CAM1_CS,-1,-1,-1);
+    
+     // Check if the ArduCAM SPI bus is OK
+    arducam_write_reg(ARDUCHIP_TEST1, 0x55, CAM1_CS);
+    temp = arducam_read_reg(ARDUCHIP_TEST1, CAM1_CS);
+    //printf("temp=%x\n",temp);
+    if(temp != 0x55) {
+        printf("SPI interface error!\n");
+        exit(EXIT_FAILURE);
+    }
+     else{
+    	   printf("SPI interface OK!\n");
+    	}
+    
+    // Change MCU mode
+    arducam_write_reg(ARDUCHIP_MODE, 0x00, CAM1_CS);
+    
+    // Check if the camera module type is OV2640
+    arducam_i2c_read(OV2640_CHIPID_HIGH, &vid);
+    arducam_i2c_read(OV2640_CHIPID_LOW, &pid);
+    if((vid != 0x26) || (pid != 0x42)) {
+        printf("Can't find OV2640 module!\n");
+        exit(EXIT_FAILURE);
+    } else {
+        printf("OV2640 detected\n");
+    }
+}
+
+int main(int argc, char** argv)
+{
+	cv::TermCriteria termcrit(cv::TermCriteria::COUNT|cv::TermCriteria::EPS,20,0.03);
+	cv::Size subPixWinSize(10,10), winSize(31,31);
+	const int MAX_COUNT = 500;
+	
+	cv::Mat raw_data, frame, prev_frame, image;
+	std::vector<cv::Point2f> points[2];
+	
+	cv::VideoWriter writer;
+	writer.open("test.avi", cv::VideoWriter::fourcc('M', 'J', 'P', 'G'), 5, cv::Size(320, 240), false);
+	
+	setup();
+	arducam_set_format(fmtJPEG);
+	arducam_init();
+	arducam_set_jpeg_size(sz320x240);
+	sleep(1);
+		
+	arducam_flush_fifo(CAM1_CS);
+	arducam_clear_fifo_flag(CAM1_CS);
+	arducam_start_capture(CAM1_CS);
+	while (!(arducam_read_reg(ARDUCHIP_TRIG, CAM1_CS) & CAP_DONE_MASK));
+	
+	//printf("Reading FIFO\n");    
+	size_t len = read_fifo_length(CAM1_CS);
+	size_t image_size = len;
+	//std::cout << image_size << std::endl;
+	if (len >= 393216){
+		printf("Over size.");
+		exit(EXIT_FAILURE);
+	} else if (len == 0 ){
+		printf("Size is 0.");
+		exit(EXIT_FAILURE);
+	} 
+	digitalWrite(CAM1_CS,LOW);  //Set CS low       
+	set_fifo_burst(BURST_FIFO_READ);
+	arducam_spi_transfers(buffer,1);//dummy read  
+	int32_t i=0;
+	while(len>4096)
+	{	 
+		arducam_transfers(&buffer[i],4096);
+		len -= 4096;
+		i += 4096;
+	}
+	arducam_spi_transfers(&buffer[i],len); 
+
+	digitalWrite(CAM1_CS,HIGH);  //Set CS HIGH
+	
+	raw_data = cv::Mat(1, image_size, CV_8UC1, (void*) buffer);
+	frame = cv::imdecode(raw_data, 0);
+	
+	cv::goodFeaturesToTrack(frame, points[1], MAX_COUNT, 0.01, 10, cv::Mat(), 3, 0, 0.04);
+	cv::cornerSubPix(frame, points[1], subPixWinSize, cv::Size(-1,-1), termcrit);
+			
+	for (int j = 0; j < 20; j++)
+	{
+		auto start = std::chrono::system_clock::now();
+		// Flush the FIFO
+		arducam_flush_fifo(CAM1_CS);    
+		// Clear the capture done flag
+		arducam_clear_fifo_flag(CAM1_CS);
+		// Start capture
+		//printf("Start capture\n");  
+		arducam_start_capture(CAM1_CS);
+		while (!(arducam_read_reg(ARDUCHIP_TRIG, CAM1_CS) & CAP_DONE_MASK)) ;
+		//printf("CAM1 Capture Done\n");
+		
+		//printf("Reading FIFO\n");    
+		size_t len = read_fifo_length(CAM1_CS);
+		size_t image_size = len;
+		//std::cout << image_size << std::endl;
+		if (len >= 393216){
+			printf("Over size.");
+			exit(EXIT_FAILURE);
+		} else if (len == 0 ){
+			printf("Size is 0.");
+			exit(EXIT_FAILURE);
+		} 
+		digitalWrite(CAM1_CS,LOW);  //Set CS low       
+		set_fifo_burst(BURST_FIFO_READ);
+		arducam_spi_transfers(buffer,1);//dummy read  
+		int32_t i=0;
+		while(len>4096)
+		{	 
+			arducam_transfers(&buffer[i],4096);
+			len -= 4096;
+			i += 4096;
+		}
+		arducam_spi_transfers(&buffer[i],len); 
+
+		digitalWrite(CAM1_CS,HIGH);  //Set CS HIGH
+		
+		raw_data = cv::Mat(1, image_size, CV_8UC1, (void*) buffer);
+		frame = cv::imdecode(raw_data, 0);
+		frame.copyTo(image);
+		
+		if (!points[0].empty())
+		{
+			std::vector<uchar> status;
+			std::vector<float> err;
+			cv::calcOpticalFlowPyrLK(prev_frame, frame, points[0], points[1],
+										status, err, winSize, 3, termcrit, 0, 0.001);
+			
+			size_t i, k;
+			for (i = k = 0; i < points[1].size(); i++)
+			{
+				points[1][k++] = points[1][i];
+				cv::circle(image, points[1][i], 3, cv::Scalar(0,255,0), -1, 8);
+			}
+		}
+		
+		//cv::Mat big_frame(480, 640, CV_8UC1);
+		//cv::resize(frame, big_frame, big_image.size(), 0, 0, cv::INTER_LINEAR);
+		writer.write(image);
+		cv::namedWindow("It works!!!!");
+		cv::imshow("It works!!!!", image);
+		cv::waitKey(1);
+		
+		auto end = std::chrono::system_clock::now();
+		auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+		std::cout << elapsed.count() << "ms" << std::endl;
+		
+		std::swap(points[1], points[0]);
+		cv::swap(prev_frame, frame);
+		//frame.copyTo(prev_frame);
+	}
+	
+	writer.release();
+	
+	return 0;
+}
---
+++ webapp/webapp.py
@@ -0,0 +1,60 @@
+from threading import Lock
+from flask import Flask, render_template, session, request
+from flask_socketio import SocketIO, emit, disconnect
+from random import random
+import psutil
+
+async_mode = None
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app, async_mode=async_mode)
+thread = None
+thread_lock = Lock()
+
+# The websocket is maintained in the background, and this
+# function outputs a random number every 5 seconds
+def background_thread():
+    """Example of how to send server generated events to clients."""
+    count = 0
+    while True:
+        socketio.sleep(5)
+        count += 1
+        number = round(random()*10, 3)
+        socketio.emit('my_response',
+                      {'data': number, 'count': count},
+                      namespace='/test')
+
+@app.route("/")
+def index():
+    num_cpus = psutil.cpu_count()
+    total_virtual_mem = psutil.virtual_memory().total
+    available_virtual_mem = psutil.virtual_memory().available
+    return render_template('index.html',
+        async_mode=socketio.async_mode,
+        num_cpus=num_cpus,
+        total_virtual_mem=total_virtual_mem,
+        available_virtual_mem=available_virtual_mem)
+
+@socketio.on('connect', namespace='/test')
+def test_connect():
+    global thread
+    with thread_lock:
+        if thread is None:
+            thread = socketio.start_background_task(target=background_thread)
+    emit('my_response', {'data': 'Connected', 'count': 0})
+
+# Ping-pong allows Javascript in the web page to calculate the
+# connection latency, averaging over time
+@socketio.on('my_ping', namespace='/test')
+def ping_pong():
+    emit('my_pong')
+
+# Notification that a client has disconnected
+@socketio.on('disconnect', namespace='/test')
+def test_disconnect():
+    print('Client disconnected', request.sid)
+
+# Run the web app
+if __name__ == '__main__':
+    socketio.run(app)
